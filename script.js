66a990561fc27f8ca44657bdbf56b88186aa1c26
RLeary04 <ryanleary04@gmail.com>
2023-11-04 14:16:55 -0700

    uhh done

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.getElementById("button");
+    const counter = document.getElementById("counter");
+
+    let count = 0;
+
+    button.addEventListener("click", function () {
+        // Increment the count and update the counter display
+        count++;
+        counter.innerText = count;
+
+        if (count >= 10) {
+            const holyMoly = document.createElement("img");
+
+            holyMoly.setAttribute("src", "https://media.tenor.com/oROQMARuS7sAAAAC/holy-moly-emoji.gif");
+
+            holyMoly.setAttribute("id", "holyMoly");
+
+            holyMoly.setAttribute("alt", "holy moly meme");
+
+            document.body.appendChild(holyMoly);
+        }
+    });
+});
